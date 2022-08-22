5af1e8442832d82885f4547aedfc8870741c68e3
GabrielFerrante <gabrielsferrante@hotmail.com>
2022-08-22 14:39:30 -0300

    agosto

--- Dataset/compactImages.py
+++ Dataset/compactImages.py
@@ -1,23 +1,53 @@
 from zipfile import ZipFile
 import os
 
-zipObj = ZipFile('zipWithObjs.zip', 'w')
+def compactWithYOLOAnn():
 
-animals = ["anta", "jaguarundi", "loboGuara", "oncaParda", "tamanduaBandeira"]
 
-for item in animals:
-    os.chdir(os.path.join("images", item))
-        
-    for filename in os.listdir(os.getcwd()):
-        if filename.endswith(".jpg"):
+    zipObj = ZipFile('zipWithObjs.zip', 'w')
+
+    animals = ["anta", "jaguarundi", "loboGuara", "oncaParda", "tamanduaBandeira"]
+
+    for item in animals:
+        os.chdir(os.path.join("images", item))
             
-            zipObj.write(filename)
-          
-        if filename.endswith(".txt"):
-            zipObj.write(filename)
+        for filename in os.listdir(os.getcwd()):
+            if filename.endswith(".jpg"):
+                
+                zipObj.write(filename)
+            
+            if filename.endswith(".txt"):
+                zipObj.write(filename)
 
-    
-    os.chdir("..")
-    os.chdir("..")
+        
+        os.chdir("..")
+        os.chdir("..")
 
-zipObj.close()
+    zipObj.close()
+
+def compactWithXMLAnn():
+    zipObj = ZipFile('zipWithObjsXML.zip', 'w')
+
+    animals = ["anta", "jaguarundi", "loboGuara", "oncaParda", "tamanduaBandeira"]
+
+    for item in animals:
+        os.chdir(os.path.join("images", item))
+            
+        for filename in os.listdir(os.getcwd()):
+            if filename.endswith(".jpg"):
+                
+                zipObj.write(filename)
+        
+        os.chdir(os.getcwd()+"/XML")
+        for filename in os.listdir(os.getcwd()):
+            if filename.endswith(".xml"):
+                zipObj.write(filename)
+
+        os.chdir("..")
+        os.chdir("..")
+        os.chdir("..")
+
+    zipObj.close()
+
+#Choice the function executable
+compactWithYOLOAnn()
